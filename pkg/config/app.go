d4799a7b320b64e5b68670163fca60f5e53b877e
hasnat.saeed <hasnat.saeed@starzplayarabia.com>
2022-04-02 14:59:33 +0500

    Updated

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -9,7 +9,7 @@ var db *gorm.DB
 
 func Connect() {
 
-	dsn := "spxUsr:vqkWWHptpCwmkgDw8twQuExC@tcp(db:3308)/go-bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "spxUsr:vqkWWHptpCwmkgDw8twQuExC@tcp(db:3306)/go-bookstore?charset=utf8mb4&parseTime=True&loc=Local"
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
